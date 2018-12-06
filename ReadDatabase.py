710ab03dfb2d4f5974a0789cf43d6f28f6fea9db
Tatjana Utz <tatjana.utz@aracom.local>
2018-12-06 12:53:25 +0100

    Structured and cleared file.

--- ReadDatabase.py
+++ ReadDatabase.py
@@ -1,192 +1,80 @@
-"""Functions to read data from a database."""
+"""Read from a database"""
 
-import os
 import sqlite3
-import numpy as np
-from Task import Task
-import Task as tsk
+import os
+import logging
 
-# path to the database
-db_path = "C:\\Users\\tatjana.utz\\Google Drive\\WS2018_19\\Programmierung"
-# define the name of the database
-db_name = "database_haecker" + ".db"
-# define the dataset (name of the table in the database)
-table_name = "Dataset2"
-# connection to the database
-db_connection = None
-
-# A Taskset
-taskset = []
+# Variables
+db_name = 'database_haecker.db'     # Name of the database, must include the '.db'-extension
+db_path = None                      # Path to the database, None if current folder should be used
+table_name = "Dataset1"             # Name of the table
+db_connection = None                # Connection to the database
 
 
-def output(msg):
-    """Output a message.
+# Open a database
+def openDb(db_name, db_path=os.path.dirname(os.path.abspath(__file__))):
+    """Open the database and return a connection to the database.
 
+    If there is no database file defined through db_name and db_path,
+    a sqlite3.OperationalError is raised.
     Keyword arguments:
-    msg -- message to output
-
-    Return values:
-    None
-    """
-    for line in msg.splitlines():
-        print("ReadDatabase: " + line)
-
-
-def openDb(db):
-    """Open a database.
-
-    Keyword arguments:
-    db -- the database
-
+    db_name -- name of the database with file ending '.db'
+    db_path -- optional, path to the database, default is the current folder
     Return values:
     connection to the database
     """
-    # Check if db exists
-    if not os.path.exists(db):      # db doesn't exist
-        output("ERROR: no database found!")
-        return
+    logging.debug("openDb(): connection to database will be established")
+    # Check if a valid path to the database is given
+    if db_path is None:
+        logging.debug("openDb(): No db_path given, current folder is used")
+        db_path = os.path.dirname(os.path.abspath(__file__))
 
-    # db exists: create connection to db
-    output("Database " + db + " exists!")
-    db_connection = sqlite3.connect(db)
-
-    # Check if successful connected
-    if db_connection is None:
-        output("ERROR: Connection to database unsuccessful!")
-        return
-
-    # Return db connection
-    return db_connection
+    full_db_name = db_path + "\\" + db_name
+    return sqlite3.connect("file:" + full_db_name + "?mode=rw", uri=True)
 
 
+# Close a database
 def closeDb(db_connection):
     """Close a database.
 
+    Before the database is closed, the changes are saved.
     Keyword arguments:
-    db_connection -- connection to a database
-
-    Return values:
-    None
+    db_connection -- connection to the database
     """
-    # Save changes of the database
+    logging.debug("closeDb(): connection to database will be closed")
     db_connection.commit()
-    # Close database
     db_connection.close()
 
 
-def getNumberOfEntries(db_connection, table_name, id_field):
-    """Return the number of entries.
+# Read a table
+def readTable(db_connection, table_name):
+    """Read a complete table from a database.
 
+    If the table does not exist in the database, a sqlite3.OperationalError is raised.
     Keyword arguments:
-    db_connection -- connection to a database
-    table_name -- name of a table
-    id_field -- name of a ID field
-
+    db_connection -- connection to the database
+    table_name -- name of the table
     Return values:
-    number of entries
+    data -- Array with the data of the table
     """
-    sql_command = "SELECT count(" + id_field + ") FROM " + table_name
+    logging.debug("readTable(): Table '" + table_name + "' will be read")
     db_cursor = db_connection.cursor()
-    db_cursor.execute(sql_command)
-    return db_cursor.fetchone()[0]
-
-
-def getData(db_connection, table_name):
-    """Get the data from a table.
-
-    Keyword arguments:
-    db_connection -- connection to a database
-    table_name -- name of a table
-
-    Return values:
-    array of data
-    """
     sql_command = "SELECT * FROM " + table_name
-    db_cursor = db_connection.cursor()
-    db_cursor.execute(sql_command)
-
     data = []
-    currentLine = db_cursor.fetchone()
-    if currentLine is None:
-        return (0)
-
-    # store data from database in array
-    while currentLine is not None:
-        data.append(currentLine)
-        currentLine = db_cursor.fetchone()
-    data = np.array(data)
-
-    # check if there is data
-    if len(data) == 0:
-        output("No data in table '" + table_name + "' available!")
-        return ()
-
+    for row in db_cursor.execute(sql_command):
+        data.append(row)
     return data
 
 
-def separateData(data):
-    """Seperate results from data.
-
-    Keyword arguments:
-    data -- the data array
-
-    Return values:
-    [data], [results]
-    """
-    i = 0
-    range = []
-    while i < (len(data[0]) - 1):   # last field for result
-        if i != 0:                  # 0 is the id field
-            range.append(i)
-        i += 1
-
-    return data[:, range], data[:, (len(data[0]) - 1)]
-
-
-def readDataset(db_connection, table_name):
-    """ Read a dataset from a dataset .
-
-    Keyword arguments:
-    db_connection -- connection to a database
-    table_name -- name of the table / name of the dataset
-
-    Return values:
-    dictionary of the dataset
-    """
-    data = getData(db_connection, table_name)
-
-    numberOfTasks = int((len(data[0]) - 2) / Task.numberOfProperties)    # -2 for Set_ID and Exit_Value
-
-    dataset = []
-    i = 0
-    while i < len(data):
-        taskset = []
-        j = 0
-        while j < numberOfTasks:
-            taskset.append(Task(data[0][1+j], data[0][2+j], data[0][3+j], data[0][4+j], data[0][5+j], data[0][6+j], data[0][7+j], data[0][8+j]))
-            j += 1
-        dataset.append(taskset)
-        i += 1
-
-    return dataset
-
-
 if __name__ == "__main__":
+    # Configure logging: format should be "LEVELNAME: Message",
+    # logging level should be DEBUG (all messages are shown)
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
-    # open database
-    db_connection = openDb(db_path + "\\" + db_name)
+    db_connection = openDb(db_name, db_path)  # Open the database
 
-    # get number of entries
-    output("Number of entries = " + str(getNumberOfEntries(db_connection, table_name, "Set_ID")))
+    data = readTable(db_connection, table_name)
+    for i in range(len(data)):
+        print(data[i])
 
-    # test Function
-    dataset = readDataset(db_connection, table_name)
-    output(str(dataset))
-
-    # get data
-    X_data, y_data = separateData(getData(db_connection, table_name))
-    output("X_data: " + str(X_data[0]))
-    output("y_data: " + str(y_data[0]))
-
-    # close database
     closeDb(db_connection)
