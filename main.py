68ed360c4f3a49d379dc9480f20dc63051647811
Tatjana Utz <tatjana.utz@aracom.local>
2018-12-06 15:15:50 +0100

    Added utilization-test

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+"""Main file of project."""
+import logging
+from utilization_test import utilization_test
+
 
 def main():
-    """This is the main function of the main module."""
-    print("This is the main function.")
+    """Main function of the project."""
+    # Perform utilzation-based schedulability analysis
+    example_taskset = [(5000, 10000), (3001, 10000), (2000, 10000)]
+    result = utilization_test(example_taskset)
+    print(result)
 
 
 if __name__ == "__main__":
+    # Configure logging: format should be "LEVELNAME: Message",
+    # logging level should be DEBUG (all messages are shown)
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
+
     main()
