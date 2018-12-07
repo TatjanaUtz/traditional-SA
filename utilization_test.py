6c99395ac28a7dd8f6784b260baa7d97abf821d4
Tatjana Utz <tatjana.utz@aracom.local>
2018-12-07 12:45:58 +0100

    Added utilization test for RM and EDF

--- utilization_test.py
+++ utilization_test.py
@@ -1,4 +1,4 @@
-"""Utilization-based Schedulability Test for fixed priority scheduler.
+"""Utilization-based Schedulability Test.
 
 A general taskset is schedulable, if and only if the total utilization is less than or equal 1:
 U = sum(C_i / T_i) <= 1 (neccessary condition)
@@ -7,8 +7,8 @@ U = sum(C_i / T_i) <= 1 (neccessary condition)
 import logging
 
 
-def utilization_test(taskset):
-    """Utilization-based schedulabilty test.
+def utilization_test_edf(taskset):
+    """Utilization-based schedulabilty test for EDF.
 
     Keyword arguments:
     taskset -- Taskset which should be tested, represented as a list of tuples:
@@ -23,6 +23,32 @@ def utilization_test(taskset):
     for i in range(len(taskset)):
         utilization += taskset[i][0] / taskset[i][1]
     if utilization <= 1:
+        logging.debug("utilization_test(): taskset is schedulable!")
+        return True
+    else:
+        logging.debug("utilization_test(): taskset is NOT scheudlable!")
+        return False
+
+
+def utilization_test_rm(taskset):
+    """Utilization-based schedulabilty test for RM.
+
+    A taskset is schedulable with RM if U <= n(2^(1/n) - 1)
+
+    Keyword arguments:
+    taskset -- Taskset which should be tested, represented as a list of tuples:
+                [(C0, T0), (C1, T2), ..., (Cn, Tn)]
+                C0 = execution time of the first task
+                T0 = period of the first task
+    Return value:
+    True/False -- schedulability result, True if taskset is schedulable, otherwise False
+    """
+    logging.debug("utilization_test(): utilization-test will be performed")
+    utilization = 0
+    for i in range(len(taskset)):
+        utilization += taskset[i][0] / taskset[i][1]
+    utilization_bound = len(taskset) * (2**(1/len(taskset)) - 1)
+    if utilization <= utilization_bound:
         logging.debug("utilization_test(): taskset is schedulable!")
         return True
     else:
