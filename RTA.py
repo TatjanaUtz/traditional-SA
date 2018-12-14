1675b7a1e35a5299c3d9ea40d4ac3cdfc1bb132b
TatjanaUtz <tatjana.utz@tum.de>
2018-12-14 10:35:16 +0100

    Added RTA and deleted unused functions

--- RTA.py
+++ RTA.py
@@ -5,161 +5,137 @@ import logging
 import math
 
 import Database as db
-from Task import Task
 from TaskSet import TaskSet
 
 
-def _compute_response_time(new_task, num_elements, check_task, taskset):
-    global _response_time_old, _response_time, _first_task
-    _response_time_old = check_task.executionTime
-    while True:
-        _curr_task = taskset[0]
-        _curr_task_idx = 0
-        _response_time = check_task.executionTime
-        for i in range(num_elements):
-            _curr_task = taskset[_curr_task_idx]
-            _response_time += math.ceil(_response_time_old / _curr_task.period) * \
-                              _curr_task.executionTime
-            _curr_task_idx += 1
+def RTA(taskset):
+    """Response Time Analysis.
 
-        # If check_task is another task then new_task we have to add the new_task here
-        if new_task != check_task:
-            _response_time += math.ceil(_response_time_old / new_task.period) * \
-                              new_task.executionTime
+    Check the schedulability of a task-set with response time analysis.
+    Calculate the response times of all tasks. The task-set is schedulable if and only if for all
+    tasks: D_i = T_i <= R_i
 
-        logging.debug("response_time = {0:d}, response_time_old = {1:d}, deadline = {2:d}"
-              .format(_response_time, _response_time_old, check_task.deadline))
+    Keyword arguments:
+        taskset -- the taskset
+    Return value:
+        True/False -- schedulability of task-set
+        -1 -- an error occurred
+    """
+    # Check input argument: must be a TaskSet
+    if not isinstance(taskset, TaskSet):  # Invalid input argument
+        logging.error("RTA/RTA(): invalid input argument, must be a TaskSet!")
+        return -1
 
-        ''' Since the response_time is increasing with each iteration, it has to be always smaller
-        then the deadline -> we can stop if we hit the deadline. '''
-        if _response_time > check_task.deadline:
-            # Task-Set is NOT schedulable
-            logging.debug("Task-Set is NOT schedulable!")
+    # Sort the task-set according to priorities: first task has highest priority
+    taskset.sort()
+    logging.debug("Task-set = " + str(taskset))
+
+    # Check schedulability of all tasks in the task-set
+    for i in range(len(taskset)):  # Iterate over all tasks
+        # Get response time of task
+        response_time = _caluclate_response_time(taskset, taskset[i])
+
+        # Check schedulability of task
+        if response_time is False or response_time > taskset[i].period:
+            # Task-set is NOT schedulable
+            logging.debug("Task-set is NOT schedulable!")
             return False
-        if _response_time_old >= _response_time:
-            if _response_time <= check_task.deadline:
-                # Task-Set is schedulable
-                logging.debug("Task-Set is schedulable! Response time = {0:d}, deadline = {1:d}"
-                      .format(_response_time, check_task.deadline))
-                return True
-        _response_time_old = _response_time
 
-
-def RTA(new_task, rq_buf):
-    global _response_time_old, _response_time, _first_task
-
-    num_elements = len(rq_buf)
-
-    ''' Assuming that each task is schedulable if it is alone, the task is accepted if the rq_buf
-    is empty.'''
-    if num_elements == 0:
-        logging.debug("num_elements == 0, Task-Set is schedulable!")
-        return True
-
-    ''' RTA.py-Algorithm
-    We assume that the existing Task-Set is schedulable without the new task. Therefore the
-    response time has to e computed for the new task and all tasks having a smaller priority then
-    the new task and for the new task. The tasks in the rq_buf are assumed to be sorted by
-    priorities. '''
-    _first_task = rq_buf[0]
-    _curr_task = _first_task
-    _curr_task_idx = 0
-    if new_task.priority < rq_buf[-1].priority:
-        logging.debug("New task hast lower priority the all other tasks, priority_new = {0:d}, \
-              priority_last = {1:d}".format(new_task.priority, rq_buf[-1].priority))
-        _response_time_old = new_task.executionTime
-        if not _compute_response_time(new_task, num_elements, new_task, rq_buf):
-            # Task-Set is not schedulable
-            logging.debug("Task-Set is not schedulable!")
-            return False
-    else:
-        # Compute response time for all tasks having priority smaller then the new task.
-        logging.debug("New task has higher or the same priority then lowest existing task, priority_new = \
-              {01:d}, priority_last = {1:d}".format(new_task.priority, rq_buf[-1].priority))
-        for i in range(num_elements):
-            _curr_task = rq_buf[_curr_task_idx]
-            _response_time_old = _curr_task.executionTime
-            if _curr_task.priority <= new_task.priority:
-                if not _compute_response_time(new_task, i, _curr_task, rq_buf):
-                    # Task-Set is not schedulable
-                    return False
-
-            # Check new_task at right position
-            if _curr_task.priority > new_task.priority and (rq_buf[_curr_task_idx+1]).priority <= \
-               new_task.priority and i > 0:
-                if not _compute_response_time(new_task, i+1, new_task, rq_buf):
-                    # Task-Set is not schedulable
-                    logging.debug("Task-Set is not schedulable!")
-                    return False
-            _curr_task_idx += 1
-    logging.debug("All Task-Sets passed the RTA.py Algorithm -> Task-Set schedulable!")
+    # All tasks are schedulable -> task-set is schedulable
+    logging.debug("Task-set is schedulable!")
     return True
 
-def test_RTA(dataset):
-    db_connection = db.openDb(db.db_name)  # Open database
-    db_cursor = db_connection.cursor()  # Create a cursor for database
 
-    # Read execution times depending on PKG and save as dictionary
-    dict_C = {}  # Empty dictionary
-    # Iterate over all table rows
-    for row in db_cursor.execute("SELECT * FROM ExecutionTimes"):
-        dict_C[row[0]] = row[1]  # Add entry "PKG" : "Execution Time"
+def _caluclate_response_time(taskset, check_task):
+    """Calculate the response time of a task.
 
-    # Variables for efficiency check
-    tp, fp, tn, fn, total = 0, 0, 0, 0, 0
+    The response time of a task i is calculated through the iterative formula:
+    start:  R0 = C_i
+    iteration:  R_(n+1) = C_i + sum( R_n / T_k * C_k)
+    stop:   R_(n+1) = R_n = R_i
+    The sums over k are always over all tasks with higher or same priority than i = hp(i).
 
-    table_name = dataset  # Name of the table
+    Keyword arguments:
+        taskset -- the task-set
+        check_task -- the task for which the response time should be calculated
+    Return value:
+        response time
+        False -- value of response time exceeds deadline of task (= period) -> task-set is not schedulable
+    """
+    # Create task-set with all task of higher or same priority as check_task = hp(i)
+    hp = TaskSet()
+    for task in taskset:  # Iterate over all tasks
+        if task.priority <= check_task.priority and task is not check_task:
+            hp.addTask(task)  # Add task to hp-set
+    logging.debug("hp(i) = " + str(hp))
 
-    # Get number of columns of the table
-    db_cursor.execute("PRAGMA table_info('" + table_name + "')")
-    numberOfColumns = len(db_cursor.fetchall())
+    r0 = check_task.execution_time  # Start with execution time of task i
 
-    # Calculate number of tasks in dataset
-    # -2 for Set_ID and Exit_Value
-    # /8 because each task has 8 properties
-    numberOfTasks = int((numberOfColumns - 2) / 8)
+    # Check if there are tasks of higher or same priority
+    if len(hp) == 0:  # check_task is task with highest priority
+        return r0  # Return response time = execution time of check_task
 
-    # Read out the table and perform the test for each taskset in table
-    for row in db_cursor.execute("SELECT * FROM " + table_name):
-        taskset = TaskSet()  # Empty taskset
-        schedulability = None
-        # Iterate over all tasks
-        for i in range(numberOfTasks):
-            C = dict_C[row[4 + i * 8]]  # Execution time of task i
-            new_task = Task(priority=row[1 + i * 8], deadline=row[2 + i * 8], quota=row[3 + i * 8],
-                            pkg=row[4 + i * 8], arg=row[5 + i * 8], period=row[6 + i * 8],
-                            numberOfJobs=row[7 + i * 8], offset=row[8 + i * 8], executionTime=C)
-            if i == (numberOfTasks - 1):  # Last task of taskset: check schedulability
-                schedulability = RTA(new_task, taskset)
-                logging.debug("Schedulability = " + str(schedulability))
-            else:
-                taskset.addTask(new_task)
+    r_old = 0
+    r_new = r0
 
-        # Evaluate Taskset
-        exitValue = row[-1]
+    while r_old != r_new:
+        r_old = r_new
+        logging.debug("R_n = " + str(r_old))
 
-        # Analyse efficiency of utilization_test
-        if schedulability is True and exitValue == 1:
+        sum_hp = 0
+        for task in hp:  # Iterate over all higher priority tasks
+            sum_hp += math.ceil(r_old / task.period) * task.execution_time
+        logging.debug("sum_hp = " + str(sum_hp))
+
+        r_new = check_task.execution_time + sum_hp
+        logging.debug("R_(n+1) = " + str(r_new))
+
+        if r_new > check_task.period:  # Deadline miss of check_task
+            return False
+
+    return r_new
+
+
+def test_dataset(dataset):
+    """Test a hole dataset.
+
+    dataset -- the dataset that should be tested.
+    """
+    taskset_list = db.get_dataset(dataset)
+
+    # Get number of task-sets in the dataset
+    number_of_tasksets = len(taskset_list)
+
+    # Variable for checking result of test
+    tp, fp, tn, fn = 0, 0, 0, 0
+
+    for i in range(number_of_tasksets):  # Iterate over all task-sets
+        taskset = taskset_list[i]
+        schedulability = RTA(taskset)
+        exit_value = taskset.exit_value
+
+        # Analyse test
+        if schedulability is True and exit_value == 1:  # True positives
             tp += 1
-        elif schedulability is True and exitValue == -1:
+        elif schedulability is True and exit_value == -1:  # False positives
             fp += 1
-        elif schedulability is False and exitValue == 1:
+        elif schedulability is False and exit_value == 1:  # False negatives
             fn += 1
-        elif schedulability is False and exitValue == -1:
+        elif schedulability is False and exit_value == -1:  # True negatives
             tn += 1
-        total += 1
 
     # Print results
-    print("---------- RESULTS OF RTA.py FOR " + table_name + " ----------")
-    print("Correct: {0:d} / {1:d} -> {2:.0f}%".format(tp + tn, total, (tp + tn) * 100 / total))
-    print("Incorrect: {0:d} / {1:d} -> {2:.0f}%".format(fp + fn, total, (fp + fn) * 100 / total))
+    print(
+        "---------- RESULTS OF RTA FOR " + dataset + " ----------")
+    print("Correct: {0:d} / {1:d} -> {2:.0f}%".format(tp + tn, number_of_tasksets,
+                                                      (tp + tn) * 100 / number_of_tasksets))
+    print("Incorrect: {0:d} / {1:d} -> {2:.0f}%".format(fp + fn, number_of_tasksets,
+                                                        (fp + fn) * 100 / number_of_tasksets))
     print("True positive (tp) = {0:d}".format(tp))
     print("False positive (fp) = {0:d}".format(fp))
     print("True negative (tn) = {0:d}".format(tn))
     print("False negative (fn) = {0:d}".format(fn))
     print("-------------------------------------------------")
-
-    db.closeDb(db_connection)  # Close database
 
 
 if __name__ == "__main__":
@@ -167,11 +143,4 @@ if __name__ == "__main__":
     # logging level should be DEBUG (all messages are shown)
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-    dataset = "Dataset3"
-
-    # Test RTA.py
-    '''test_RTA("Dataset1")
-    test_RTA("Dataset2")
-    test_RTA("Dataset3")
-    test_RTA("Dataset4")
-    test_RTA("Dataset5")'''
+    test_dataset("Dataset5")
