9b4c7ac254e093d294a7102e14f9aa810dc2ce8c
TatjanaUtz <tatjana.utz@tum.de>
2019-02-22 12:33:17 +0100

    Added logging

--- main.py
+++ main.py
@@ -299,6 +299,6 @@ def main():
 if __name__ == "__main__":
     # Configure logging: format should be "LEVELNAME: Message",
     # logging level should be DEBUG (all messages are shown)
-    # logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
     main()
