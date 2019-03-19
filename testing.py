fd1f1fc94e947a83827baa2bafc816289cce723f
TatjanaUtz <tatjana.utz@tum.de>
2019-03-19 14:45:47 +0100

    Deleted unused files

--- testing.py
+++ testing.py
@@ -4,17 +4,17 @@ import logging
 import simsogui
 
 from database_interface import Database
-from workload import het_workload_test
+from utilization import basic_utilization_test
 
 
 def test_schedulability_test():
     """Main function for testing of single schedulability tests."""
     my_database = Database()
     taskset_46429 = my_database.get_taskset(taskset_id=46429)
-    print(het_workload_test(taskset_46429))
+    print("Rückgabe: ", basic_utilization_test(taskset_46429))
 
     taskset_563782 = my_database.get_taskset(taskset_id=563782)
-    print(het_workload_test(taskset_563782))
+    print("Rückgabe: ", basic_utilization_test(taskset_563782))
 
 
 def start_simso():
@@ -27,4 +27,4 @@ if __name__ == "__main__":
     # logging level should be DEBUG (all messages are shown)
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
-    start_simso()
+    test_schedulability_test()
