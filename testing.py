e03f358f5bf3c4693c4216d862ecf9e9e4c3cd54
TatjanaUtz <tatjana.utz@gmail.com>
2019-05-02 09:57:24 +0200

    Added function for calculating average execution time of schedulability tests

--- testing.py
+++ testing.py
@@ -33,10 +33,32 @@ def start_simso():
     """Start SimSo GUI."""
     simsogui.run_gui()
 
+def time_per_taskset():
+    # load the dataset
+    my_database = Database(os.getcwd(), "panda_v3.db")
+    dataset = my_database.read_table_taskset()
+
+    # create empty list for times
+    times = []
+
+    for taskset in dataset: # iterate over all task-sets
+        # do schedulability analysis
+        start_t = time.time()
+        simulate(taskset)
+        end_t = time.time()
+
+        # add time
+        times.append(end_t - start_t)
+
+    # calculate average time
+    average_t = sum(times) / len(times)
+    print("SIMULATION -- Average time per task-set: %f s" %(average_t))
+
+
 
 if __name__ == "__main__":
     # Configure logging: format should be "LEVELNAME: Message",
     # logging level should be DEBUG (all messages are shown)
     logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
-    test_schedulability_test()
+    time_per_taskset()
